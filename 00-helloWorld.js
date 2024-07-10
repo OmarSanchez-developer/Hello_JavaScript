cb08d37e6a90bdef310a84e914c0d61c9e4432b4
Omar  Sánchez <ing_omarglez@hotmail.com>
2024-07-10 15:45:34 -0600

    Inicio del Curso para aprender el lenguaje de programación JavaScript desde cero y para principiantes
    Curso para aprender el lenguaje de programación JavaScript desde cero y para principiantes impartido por Brais - Moure

---
+++ 00-helloWorld.js
@@ -0,0 +1,21 @@
+// Esta linea es un comentario simple del curso nuevo desde cero de JavaScript impartido por Brais - MoureDev
+
+/*
+Este es 
+un comentario
+en varias lineas
+*/
+
+console.log("¡Hola, JavaScript!")
+/*
+console.log("¡Hola, JavaScript!")
+*/
+
+console.log("5")
+console.log(5)
+console.log(5 + 2)
+console.log(5 * 2)
+console.log(5 / 2)
+console.log(5 % 2)
+console.log(5 ** 2)
+
---
+++ README.md
@@ -0,0 +1,8 @@
+Hello JavaScript
+Python
+
+Curso para aprender el lenguaje de programación JavaScript desde cero y para principiantes
+
+
+Proyecto realizado durante emisiones en directo desde Twitch
+Si consideras útil el curso, apóyalo haciendo "★ Star" en el repositorio. ¡Gracias!
